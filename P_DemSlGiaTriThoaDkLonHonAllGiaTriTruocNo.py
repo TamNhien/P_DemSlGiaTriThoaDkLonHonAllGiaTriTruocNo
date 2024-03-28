36dfea372845015a342edc63f837697498e16e11
TamNhien <canhthien91@gmail.com>
2024-03-28 20:13:08 +0700

    Add project files.

---
+++ P_DemSlGiaTriThoaDkLonHonAllGiaTriTruocNo.py
@@ -0,0 +1,4 @@
+﻿L = list(map(int, input("Nhập vào danh sách các số nguyên, cách nhau bởi dấu phẩy: ").split(',')))
+count = sum(1 for i in range(1, len(L)) if all(L[i] > x for x in L[:i]))
+print("Số lượng giá trị thỏa điều kiện là: ", count)
+
